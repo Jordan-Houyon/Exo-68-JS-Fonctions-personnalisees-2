2bfadccd6954f1d1835b74eb85230d2d7a780b44
jordan houyon <116800565+Jordan-Houyon@users.noreply.github.com>
2022-11-21 15:53:43 +0100

    exo 68 ok

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-
-let paragraphe = document.querySelectorAll("p");
+// je reécupère les valeurs stockées dans mes paragraphes//
+let paragraphe = document.getElementsByTagName("p");
 let random;
 
+// fonction qui va générér un aléatoire entre 1 et 100 //
 function randomgenerate(math) {
+
+    //génération d'un aléatoire entre 1 et 100 , sans arrondi et sans partie décimale//
     random = Math.trunc(Math.random() * 100);
+
+    // je renvoie la valeur stockée dans random ici un aléatoire donc //
+
     return random
 }
+
+// grâce à une boucle, on recupère  un nombre aléatoire dans le innerHTML de chaque élément récupéré
 
 for( let i = 0; i < paragraphe.length; i++){
     randomgenerate()
